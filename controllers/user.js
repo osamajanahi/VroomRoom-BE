a39a7b29df1d25f189fd2158b87a5d295658f8c9
m0hx777 <m0hx@protonmail.com>
2023-12-28 13:50:42 +0300

    user showget/update

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,24 @@
 const {User} = require('../models/User');
 
 
+exports.user_show_get = (req, res) => {
+    console.log(req.query.id);
+    user.findById(req.query.id)
+    .then((user) => {
+        res.json({user})
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
+exports.user_update_put = (req, res) => {
+    console.log(req.body.id);
+    user.findByIdAndUpdate(req.body.id, req.body)
+    .then(() => {
+      res.redirect("/user/index");
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
