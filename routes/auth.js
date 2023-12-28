f030907f59deabab7da826e01c4ede1a2886d1cd
m0hx777 <m0hx@protonmail.com>
2023-12-28 10:38:10 +0300

    auth.js route

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = require("express").Router();
+
+
+router.use(express.json());
+
+
+// Import Auth Controller
+const authCtrl = require("../controllers/auth");
+const isLoggedIn = require("../helper/isLoggedIn");
+
+
+
+// Routes
+router.post("/signup", authCtrl.auth_signup_post);
+router.post("/signin", authCtrl.auth_signin_post);
+
+
+
+// Exports
+module.exports = router;
