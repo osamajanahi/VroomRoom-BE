871365e22194937f483cb29e13e0f387f0ef42af
osamajanahi <osamaj12@gmail.com>
2024-01-03 09:24:43 +0300

    wishlist and post modification

--- routes/post.js
+++ routes/post.js
@@ -14,7 +14,7 @@ router.use(express.json());
 router.post('/add', upload.array('image',5), postCtrl.post_create_post);
 router.get('/index', postCtrl.post_index_get);
 router.get('/edit', postCtrl.post_detail_get);
-router.post('/edit', postCtrl.post_edit_post);
+router.post('/edit', upload.array('image',5), postCtrl.post_edit_post);
 router.get('/delete', postCtrl.post_delete_get);
 router.get('/detail', postCtrl.post_detail_get);
 
